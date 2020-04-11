445d58dc4cbb8d80e812c7178f2861d09c4cca96
negin sharif <negysharif@gmail.com>
2020-04-11 20:13:23 +0200

    toolbar collapse

--- app/src/main/java/com/example/music/data/requests/api/LastfmApi.java
+++ app/src/main/java/com/example/music/data/requests/api/LastfmApi.java
@@ -4,6 +4,7 @@ import androidx.lifecycle.LiveData;
 
 import com.example.music.data.model.albumsinfo.Album;
 import com.example.music.data.model.artistsearch.ArtistSearch;
+import com.example.music.data.model.topalbums.ArtistsTopAlbums;
 
 import java.util.List;
 import java.util.Observable;
@@ -16,15 +17,20 @@ import retrofit2.http.Query;
 public interface LastfmApi {
 
 
- //   http://ws.audioscrobbler.com/2.0/?method=artist.search&artist=cher&api_key=YOUR_API_KEY&format=json
+    //   http://ws.audioscrobbler.com/2.0/?method=artist.search&artist=cher&api_key=YOUR_API_KEY&format=json
     @GET("?method=artist.search&format=json")
     Call<ArtistSearch> getArtists(@Query("artist") String artistName,
-                                                 @Query("api_key") String apiKey);
+                                  @Query("api_key") String apiKey);
 
-   // ?method=album.getinfo&api_key=YOUR_API_KEY&artist=Cher&album=Believe&format=json
+    // ?method=album.getinfo&api_key=YOUR_API_KEY&artist=Cher&album=Believe&format=json
     @GET("?method=album.getinfo&format=json")
     Call<List<Album>> getAlbums(@Query("api_key") String apiKey,
                                 @Query("artist") String artistName,
                                 @Query("album") String albumName);
+
+    //?method=artist.gettopalbums&artist=cher&api_key=YOUR_API_KEY&format=json
+    @GET("?method=artist.gettopalbums&format=json")
+    Call<ArtistsTopAlbums> getTopAlbums(@Query("artist") String artistName,
+                                        @Query("api_key") String apiKey);
 
 }
